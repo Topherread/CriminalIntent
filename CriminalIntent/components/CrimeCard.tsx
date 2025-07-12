9333aa192e40a9a0927d6abd58ef6e73f8aac070
Topherread <106223142+Topherread@users.noreply.github.com>
2025-07-12 11:32:10 -0600

    before theme

--- CriminalIntent/components/CrimeCard.tsx
+++ CriminalIntent/components/CrimeCard.tsx
@@ -6,13 +6,13 @@ const CrimeCard = ({ crime }) => {
   const router = useRouter();
   
   return (
-    <Pressable onPress={() => router.push("/crime")}>
+    <Pressable onPress={() => router.push("/crime")} style={{ padding: 16, borderBottomWidth: 1, borderColor: '#ccc', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between' }}>
         <View>
             <Text>{crime.title}</Text>
             <Text>Details: {crime.details}</Text>
             <Text>Date: {crime.date}</Text>
-            <MaterialCommunityIcons name="handcuffs" size={24} color="black" />
         </View>
+        <MaterialCommunityIcons name="handcuffs" size={24} color="black" />
     </Pressable>
   );
 }
